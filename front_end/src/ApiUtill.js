894046690b125d3df287b43ba05b94b46ab12c38
anion <1005128408@qq.com>
2024-10-23 10:16:36 +0800

    Change root url

--- front_end/src/ApiUtill.js
+++ front_end/src/ApiUtill.js
@@ -1,5 +1,5 @@
 export default class ApiUtill{
-    static url_root='http://127.0.0.1:5000/'
+    static url_root='http://api.wuhumodeltest.com/'
     
     static url_gpt="ask_gpt"
     static url_ourmodel="ask_ourmodel"
