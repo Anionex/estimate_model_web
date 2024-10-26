3539c8e079cf233c537af6b6b8150642478f6a56
anion <1005128408@qq.com>
2024-10-26 10:48:52 +0800

    chore: 生产开发环境自动切换

--- front_end/src/ApiUtill.js
+++ front_end/src/ApiUtill.js
@@ -1,9 +1,9 @@
 export default class ApiUtill{
-    // static url_root='http://api.wuhumodeltest.com/'
-    static url_root='http://127.0.0.1:5000/'
+    static url_root = process.env.NODE_ENV === 'production' 
+        ? 'http://api.wuhumodeltest.com/'
+        : 'http://127.0.0.1:5000/'
     static url_gpt="ask_gpt"
     static url_ourmodel="ask_ourmodel"
     static url_xxmodel="ask_xxmodel"
     static url_rating="rate"
 }
-// npm install react-router-dom axios 漏了
