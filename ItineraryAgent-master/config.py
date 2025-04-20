7e493d51ca39c4ea6ef80e873d3f1a6238e493f6
anion <1005128408@qq.com>
2025-04-21 00:08:23 +0800

    feat: move the intro prompt to react prompt

--- ItineraryAgent-master/config.py
+++ ItineraryAgent-master/config.py
@@ -13,6 +13,7 @@ SEARCH_REMAIN_NUM = 3
 ## agent settings
 STOP_WORD = ">"
 PLANNER_BEGIN_WORDS = "<Analysis:To create a comprehensive travel plan, I should first determine some basic information for this trip, including budget, number of days, cities and their visiting time, etc.If not provided, I'll set reasonably based on context.Then I'll colect the transpotation information in order to connect the cities.>"
+PLANNER_BEGIN_WORDS = ""
 
 MAX_CHECK_ITER = 6
 MAX_SCRATCHPAD_ITEMS = 20
