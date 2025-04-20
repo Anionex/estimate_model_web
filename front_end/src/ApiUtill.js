74b138fb9e4078ed8bb40c5305154eeea49a7263
anion <1005128408@qq.com>
2025-04-20 21:41:35 +0800

    feat: use https for url_root

--- front_end/src/ApiUtill.js
+++ front_end/src/ApiUtill.js
@@ -1,6 +1,6 @@
 export default class ApiUtill{
     static url_root = process.env.NODE_ENV === 'production' 
-        ? 'http://api.wuhumodeltest.com/'
+        ? 'https://api.wuhumodeltest.com/'
         : 'http://127.0.0.1:5000/'
     static url_gpt="ask_gpt"
     static url_ourmodel="ask_ourmodel"
